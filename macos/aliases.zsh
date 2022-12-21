1e8ddd13f43293651bd923f088d4f9a40626782c
Giovani Zamboni <gzamboni@gmail.com>
2022-12-20 23:55:19 -0300

    fix: install process
    Signed-off-by: Giovani Zamboni <gzamboni@gmail.com>

--- macos/aliases.zsh
+++ macos/aliases.zsh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+if [ "$(uname -s)" != "Darwin" ]; then
+	exit 0
+fi
+
 alias airport="/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport"
 alias afk="open -a /System/Library/CoreServices/ScreenSaverEngine.app"
