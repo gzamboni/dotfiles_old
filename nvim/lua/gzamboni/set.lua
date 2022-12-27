f38e6683a96fc2c4626184a12397a43f7e9d88bb
Giovani Zamboni <gzamboni@gmail.com>
2022-12-27 00:40:52 -0300

    fix: nvim tree toggle
    Signed-off-by: Giovani Zamboni <gzamboni@gmail.com>

--- nvim/lua/gzamboni/set.lua
+++ nvim/lua/gzamboni/set.lua
@@ -3,8 +3,8 @@ vim.g.mapleader = " "
 vim.opt.nu = true
 vim.opt.relativenumber = true
 
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 
@@ -29,4 +29,9 @@ vim.opt.isfname:append("@-@")
 vim.opt.updatetime = 50
 
 vim.opt.colorcolumn = "80"
+
+vim.opt.ruler = true
+vim.opt.spell = true
+
+vim.opt.backspace = { "indent", "eol", "start" }
 
