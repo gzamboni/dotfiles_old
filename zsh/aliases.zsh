7b33876484fcabe3290bbf4694ea001961fa567b
Giovani Zamboni <gzamboni@gmail.com>
2023-03-27 11:46:16 -0300

    feat: openai models
    Signed-off-by: Giovani Zamboni <gzamboni@gmail.com>

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,3 +2,7 @@
 alias reload!='exec "$SHELL" -l'
 alias vim=nvim
 alias ls="exa --icons --group-directories-first"
+
+function gptmodels() {
+	curl -s https://api.openai.com/v1/models -H "Authorization: Bearer $OPENAI_API_KEY" | jq '.data[] | select(.id | contains("'$1'")) | .id'
+}
