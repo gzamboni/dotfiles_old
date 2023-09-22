ebc2060d814ee8ccd8503549275f643b2f15c556
Giovani Zamboni <g@zamboni.dev>
2023-09-22 15:40:26 -0300

    feat: add helix and gcloud aliases
    Signed-off-by: Giovani Zamboni <g@zamboni.dev>

--- gcloud/aliases.zsh
+++ gcloud/aliases.zsh
@@ -1,6 +1,19 @@
 #!/bin/zsh
 # Path: gcloud/aliases.zsh
 
-export CLOUDSDK_PYTHON=python3
+export CLOUDSDK_PYTHON=/Users/gzamboni/.asdf/installs/python/3.11.3/bin/python
 
 alias gcloud='gcloud --quiet'
+alias gcp='gcloud'
+alias gcp-login='gcloud auth login'
+alias gcp-logout='gcloud auth revoke'
+alias gcp-whoami='gcloud auth list'
+alias gcp-whoami-active='gcloud auth list --filter=status:ACTIVE'
+alias gcp-whoami-active-account='gcloud auth list --filter=status:ACTIVE --format="value(account)"'
+alias gcpp='gcloud projects'
+alias gcppl='gcloud projects list'
+alias gcpps='gcloud projects set'
+alias gcpa='gcloud auth'
+alias gcpal='gcloud auth list'
+alias gcpac='gcloud auth configure-docker'
+alias gcpa-activate='gcloud auth activate-service-account'
